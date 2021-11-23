d4b2aa6fb1c33982c511df20f99292b69123241b
tanvircancode <tanahmchw241996@gmail.com>
2021-11-23 23:54:01 +0600

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -37,32 +37,69 @@ function Footer() {
           </div>
           <div className="footer-link-items">
             <h2>Contact Us</h2>
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/sign-up">Testimonials</Link>
-            <Link to="/sign-up">Career</Link>
-            <Link to="/sign-up">Invertors</Link>
-            <Link to="/sign-up">Terms of service</Link>
+            <Link to="/">Contact</Link>
+            <Link to="/">Support</Link>
+            <Link to="/">Destinations</Link>
+            <Link to="/">Sponsorships</Link>
           </div>
         </div>
         <div className="footer-link-wrapper">
           <div className="footer-link-items">
             <h2>Videos</h2>
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/sign-up">Testimonials</Link>
-            <Link to="/sign-up">Career</Link>
-            <Link to="/sign-up">Invertors</Link>
-            <Link to="/sign-up">Terms of service</Link>
+            <Link to="/">Submit Video</Link>
+            <Link to="/">Ambassadors</Link>
+            <Link to="/">Agency</Link>
+            <Link to="/">Influencer</Link>
           </div>
           <div className="footer-link-items">
             <h2>Social Media</h2>
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/sign-up">Testimonials</Link>
-            <Link to="/sign-up">Career</Link>
-            <Link to="/sign-up">Invertors</Link>
-            <Link to="/sign-up">Terms of service</Link>
+            <Link to="/sign-up">Instagram</Link>
+            <Link to="/sign-up">Facebook</Link>
+            <Link to="/sign-up">Youtube</Link>
+            <Link to="/sign-up">Twitter</Link>
           </div>
         </div>
       </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              TRVL <i className="fab fa-typo3" />
+            </Link>
+          </div>
+          <small className="website-rights">TRVL@2020</small>
+          <div className="social-icons">
+            <Link
+              to="/"
+              className="social-icon-link facebook"
+              target="_blank"
+              aria-label="Facebook">
+              <i className="fab fa-facebook-f" />
+            </Link>
+            <Link
+              className="social-icon-link instagram"
+              target="_blank"
+              aria-label="Instagram"
+              to="/">
+              <i class="fab fa-instagram" />
+            </Link>
+            <Link
+              to="/"
+              className="social-icon-link linkedin"
+              target="_blank"
+              aria-label="LinkedIn">
+              <i class="fab fa-linkedin"></i>
+            </Link>
+            <Link
+              className="social-icon-link twitter"
+              target="_blank"
+              aria-label="Twitter"
+              to="/">
+              <i class="fab fa-twitter"></i>
+            </Link>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
